310365403eafee73ab5a3079bd99ffe873626019
theoderic <onipetheoderic@gmail.com>
2019-08-13 11:25:49 +0100

    init create-react-library@2.6.7

---
+++ example/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import ExampleComponent from 'theoderic_responsive_carousel'
+
+export default class App extends Component {
+  render () {
+    return (
+      <div>
+        <ExampleComponent text='Modern React component module' />
+      </div>
+    )
+  }
+}
